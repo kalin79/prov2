9923bbe4312371f80236d07e8316f51c7a1c0057
Carlos Espinoza <c.augusto.espinoza@gmail.com>
2025-04-24 10:06:59 -0500

    8 productos listos

--- public/js/mainInterno.js
+++ public/js/mainInterno.js
@@ -1,3 +1,56 @@
+
+$(document).ready(function () {
+    $(".owlCarruselInterna1").owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        navText: [
+            '<img src="../images/flecha2.png" alt="Prev" />',
+            '<img src="../images/flecha1.png" alt="Next" />'
+        ],
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 1,
+                nav: true
+            },
+            1000: {
+                items: 1,
+                nav: true,
+            }
+        }
+    });
+
+
+    $(".owlCarruselInterna2").owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        navText: [
+            '<img src="../images/flecha2.png" alt="Prev" />',
+            '<img src="../images/flecha1.png" alt="Next" />'
+        ],
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 1,
+                nav: true
+            },
+            1000: {
+                items: 1,
+                nav: true,
+            }
+        }
+    });
+});
+
+
 function mostrarAcordion(id, idClose) {
 
     let tabActive = document.querySelector(`.tab-${id}`);
